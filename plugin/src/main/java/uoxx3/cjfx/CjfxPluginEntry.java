42ddb343e9413079109e41b944d865dd2b255a03
Brian Alvarez <brianAlvarezPersonal@outlook.com>
2023-12-19 19:17:31 -0600

    feat(project): Base configuration

---
+++ plugin/src/main/java/uoxx3/cjfx/CjfxPluginEntry.java
@@ -0,0 +1,19 @@
+package uoxx3.cjfx;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.jetbrains.annotations.NotNull;
+
+public class CjfxPluginEntry implements Plugin<Project> {
+	
+	/**
+	 * Apply this plugin to the given target object.
+	 *
+	 * @param project The target project
+	 */
+	public void apply(@NotNull Project project) {
+		// Register all configurations
+		
+	}
+	
+}
